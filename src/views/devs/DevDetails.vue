9a5c8254a3d554de7c5f329f98332cce6b532092
afrmounir <60487182+afrmounir@users.noreply.github.com>
2023-05-16 16:42:11 +0200

    feat: build the dev details component

--- src/views/devs/DevDetails.vue
+++ src/views/devs/DevDetails.vue
@@ -1,8 +1,41 @@
-<template>
-    <h2>Dev Details</h2>
-    <RouterView></RouterView>
-    <RouterLink to="/devs/d1/contact">Contact</RouterLink>
-</template>
-
 <script setup>
+import { computed } from 'vue'
+import { useDevsStore } from '../../stores/devs'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const store = useDevsStore()
+const props = defineProps({ id: String })
+
+const selectedDev = store.filteredDevs.find((dev) => dev.id === props.id)
+
+const fullName = computed(() => `${selectedDev.firstName} ${selectedDev.lastName}`)
+const devContactLink = computed(() => `${route.path}/contact`)
+const areas = computed(() => selectedDev.areas)
+const hourlyRate = computed(() => selectedDev.hourlyRate)
+const description = computed(() => selectedDev.description)
 </script>
+
+<template>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>Taux Horaire: {{ hourlyRate }}€</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Intéressé? Entrez immédiatement en contact!</h2>
+        <base-button link :to="`${devContactLink}`">Contactez ce développeur</base-button>
+      </header>
+      <RouterView></RouterView>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
+</template>
