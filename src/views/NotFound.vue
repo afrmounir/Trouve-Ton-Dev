ad8c0642d2993c23083f76a91cf3510cd54db116
afrmounir <afrmounir@gmail.com>
2023-05-24 10:11:25 +0200

    feat: improve NotFound view

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,9 @@
 <template>
-  <main>
+  <base-card>
     <h2>404 Not Found</h2>
-  </main>
+    <p>
+      La page que vous rechercher n'existe pas - Découvrez tout les
+      <router-link to="/devs">devs</router-link>
+    </p>
+  </base-card>
 </template>
-
-<script setup>
-</script>
